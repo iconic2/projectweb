129e6de8cba837e584243c62d677bbb95222d960
sherlock123401234 <97603861+sherlock123401234@users.noreply.github.com>
2022-04-09 22:01:32 +0200

    Update forms.py

--- project/klanten/forms.py
+++ project/klanten/forms.py
@@ -1,2 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import data_required, email
+
+class klantlogin(FlaskForm):
+    email = StringField('Voer hier uw emailadres in',validators=[data_required(),email()])
+    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
+    submit = SubmitField('Inloggen')
+
+
+ 
