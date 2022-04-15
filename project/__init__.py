e65d51dd4518e28f98f6c7c6f7e9d22172d37f09
sherlock123401234 <97603861+sherlock123401234@users.noreply.github.com>
2022-04-15 17:34:41 +0200

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -3,16 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 
+# SECRET KEY VOOR PREVENTIE CSRF
 app.config['SECRET_KEY']='ADFADSFAHFKA&&&&*D11'
+
+# AANGEVEN WAAR DATABASE STAAT
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view("login")
-login_manager.login_message = "U moet eerst inloggen."
+#login_manager.login_view("login")
+#login_manager.login_message = "U moet eerst inloggen."
 
 db = SQLAlchemy(app)
