cf82117a9ef8fddcfbc4ad45fe51b40688b2b23a
sherlock123401234 <97603861+sherlock123401234@users.noreply.github.com>
2022-04-16 12:54:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template,redirect, url_for, Blueprint,flash
 from flask_wtf import FlaskForm
-from project.forms import Klantlogin,Docentlogin
+from project.forms import Klantlogin,Docentlogin, Registreren, RegistrerenDocent
 from project.models import *
 from project import app,login_manager,db
 from flask_login import login_user, logout_user
@@ -20,10 +20,46 @@ from sqlalchemy.orm import sessionmaker
 def index():
     return render_template("home.html")
 
-@app.route("/register")
-def registreer():
 
-    return render_template('home.html')
+
+
+
+@app.route("/registreer",methods=['GET','POST'])
+def registreer():
+    form = Registreren()
+    #print(form.validate_email(form.email))
+    #print(form.validate_username(form.gebruikersnaam))
+    if form.validate_on_submit():
+
+        # Als email en gebruikersnaam niet bestaan oftewel is None:
+        if form.validate_email(form.email) == None and form.validate_username(form.gebruikersnaam) == None:
+        
+            user = Gebruikers(emailadres=form.email.data,gebruikersnaam=form.gebruikersnaam.data,wachtwoord=form.wachtwoord.data,is_docent=0)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        
+        return render_template('registreer.html',form=form)
+    return render_template('registreer.html',form=form)
+
+
+@app.route("/docent/registreer",methods=['GET','POST'])
+def registreerdocent():
+    form = RegistrerenDocent()
+    #print(form.validate_email(form.email))
+    #print(form.validate_username(form.gebruikersnaam))
+    if form.validate_on_submit():
+
+        # Als email en gebruikersnaam niet bestaan oftewel is None:
+        if form.validate_email(form.email) == None and form.validate_username(form.gebruikersnaam) == None:
+        
+            user = Gebruikers(emailadres=form.email.data,gebruikersnaam=form.gebruikersnaam.data,wachtwoord=form.wachtwoord.data,is_docent=1)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('logindocent'))
+        
+        return render_template('registreer.html',form=form)
+    return render_template('registreer.html',form=form)
 
 @app.route("/inloggen", methods=["GET","POST"])
 def login():
@@ -40,7 +76,6 @@ def login():
       
     return render_template('login.html',form=form)
 
-
 @app.route("/docent/inloggen", methods=["GET","POST"])
 def logindocent():
     """
@@ -53,11 +88,11 @@ def logindocent():
 
     form = Docentlogin()
     if form.validate_on_submit():
-       # voor alleen de klanten checken of gebruiker bestaat:
+       # bestaat de gebruiker?:
        user = Gebruikers.query.filter_by(emailadres=form.email.data).first()
 
        # gekozen om te kijken of de gebruiker ook een docent is
-       # deze query bekijkt wat de waarde is van de kolom is_docent bij het ingevuld email adres.
+       # deze query bekijkt wat de waarde is van de kolom is_docent bij het ingevuld emailadres.
        docent = session.query(Gebruikers.is_docent).filter(Gebruikers.emailadres==form.email.data).first()
 
 
@@ -108,76 +143,3 @@ def gegevens_bewerken():
 # Alleen runnen als de code in dit bestand direct is uitgevoerd en niet als module is aangeroepen:
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-'''from flask import Flask, render_template,redirect, url_for
-from flask_wtf import FlaskForm
-from project.forms import klantlogin
-from project.models import *
-from project import app,login_manager,db
-from flask_login import login_user, logout_user
-
-
-
-@app.route("/")
-def index():
-    return render_template("home.html")
-
-@app.route("/register")
-def registreer():
-
-    return render_template('home.html')
-
-
-@app.route("/inloggen", methods=["GET","POST"])
-def login():
-    form = klantlogin()
-    if form.validate_on_submit():
-       user = Klanten.query.filter_by(emailadres=form.email.data).first()
-       print('USER IS' , user)
-       if user.check_password(form.wachtwoord.data) and user is not None:
-          login_user(user)
-          print('JAAAAAAAAAAAA')
-       return redirect(url_for("registreer"))
-    return render_template('login.html',form=form)
-
-
-
-
-
-@app.route("/leerlingen")
-def leerlingen():
-    return render_template("leerlingen.html")
-# leerlingen zullen alle acties die ze mogen doen, op deze pagina kunnen doen.
-
-
-@app.route("/docenten")
-def docenten():
-    return render_template("docenten.html")
-
-
-
-
-######## HIERONDER DE VIEWS ############
-@app.route("/roosters_wijzigen")
-def roosters_wijzigen():
-    return render_template('roosters_wijzigen.html')
-
-@app.route("/les_toevoegen")
-def les_toevoegen():
-    return render_template('les_toevoegen.html')
-
-@app.route("/taal_toevoegen")
-def taal_toevoegen():
-    return render_template('taal_toevoegen.html')
-
-@app.route("/gegevens_bewerken")
-def gegevens_bewerken():
-    return render_template('gegevens_bewerken.html')
-
-
-# Alleen runnen als de code in dit bestand direct is uitgevoerd en niet als module is aangeroepen:
-if __name__ == "__main__":
-    app.run(debug=True)
-    
-'''
