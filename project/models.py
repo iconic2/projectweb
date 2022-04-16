8207b25d258225ecb41446ac87727d15fa366466
sherlock123401234 <97603861+sherlock123401234@users.noreply.github.com>
2022-04-16 15:21:15 +0200

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,11 +1,12 @@
 
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 
 from flask import Flask
 from project import login_manager,db,app
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from sqlalchemy import UniqueConstraint
 
 # haal id op van gebruiker
 @login_manager.user_loader
@@ -16,11 +17,11 @@ def load_user(user_id):
 
 class Gebruikers(db.Model,UserMixin):
     __tablename__ = 'gebruikers'
-    id = db.Column(db.Integer, primary_key=True)#AUTOINCREMENT NOG TOEVOEGEN
+    id = db.Column(db.Integer, primary_key=True)
     emailadres = db.Column(db.VARCHAR(50))
-    gebruikersnaam = db.Column(db.VARCHAR(25))#UNIQUE
-    wachtwoord = db.Column(db.VARCHAR(128))#UNIQUE
-    is_docent = db.Column(db.Integer, default=0)#UNIQUE
+    gebruikersnaam = db.Column(db.VARCHAR(25))
+    wachtwoord = db.Column(db.VARCHAR(128))
+    is_docent = db.Column(db.Integer, default=0)
 
     def __init__(self, emailadres,gebruikersnaam,wachtwoord,is_docent):
         self.emailadres = emailadres
@@ -32,27 +33,27 @@ class Gebruikers(db.Model,UserMixin):
         return check_password_hash(self.wachtwoord, password)
 
 
-
-
 class Lessen(db.Model,UserMixin):
     __tablename__ = 'lessen'
-    id = db.Column(db.Integer, primary_key=True)#AUTOINCREMENT NOG TOEVOEGEN
-    docent_id = db.Column(db.Integer, db.ForeignKey('gebruikers.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    docent_id = db.Column(db.Integer, db.ForeignKey('gebruikers_lessen.g_id'))
     taal_id = db.Column(db.Integer,db.ForeignKey('talen.id'))
     start = db.Column(db.VARCHAR(20))
-    locatie = db.Column(db.VARCHAR(20))# TOEVOEGEN MET UNIQUE
+    locatie = db.Column(db.VARCHAR(20))
 # Database connectie
+
+
+class gebruikers_Lessen(db.Model,UserMixin):
+    __tablename__ = 'gebruikers_lessen'
+    g_id = db.Column(db.Integer,db.ForeignKey('gebruikers.id'), primary_key=True) 
+    l_id = db.Column(db.Integer,db.ForeignKey('lessen.id'), primary_key=True )
+
 
 class programmeer_talen(db.Model,UserMixin):
     __tablename__ = 'talen'
-    id = db.Column(db.Integer, primary_key=True)#AUTOINCREMENT NOG TOEVOEGEN
+    id = db.Column(db.Integer, primary_key=True)
     naam = db.Column(db.VARCHAR(20))
-    #UNIQUE TOEVOEGEN
-
-class Klant_Lessen(db.Model,UserMixin):
-    __tablename__ = 'klant_lessen'
-    k_id = db.Column(db.Integer,db.ForeignKey('gebruikers.id'), primary_key=True) 
-    L_id = db.Column(db.Integer,db.ForeignKey('lessen.id'), primary_key=True )#AUTOINCREMENT NOG 
-
+    UniqueConstraint('naam','id',name='uc1')
+    
 
 db.create_all()
