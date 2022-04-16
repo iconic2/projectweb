93ede57b74e9543d7e961cb46ff0d69f872ef99c
sherlock123401234 <97603861+sherlock123401234@users.noreply.github.com>
2022-04-16 12:56:16 +0200

    Update forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,8 +1,13 @@
-from email.policy import default
 from flask_wtf import FlaskForm
 from wtforms import (StringField, SubmitField,
-            PasswordField, DateField, EmailField)
-from wtforms.validators import DataRequired, email_validator as email
+            PasswordField, DateField, EmailField,IntegerField)
+#from wtforms.validators import DataRequired, email_validator as email
+from wtforms.validators import data_required, email, EqualTo, ValidationError
+from project.models import Gebruikers
+from flask import flash
+
+
+
 
 class RoostersForm(FlaskForm):
     voornaam = StringField("Voornaam", validators=[DataRequired()])
@@ -26,12 +31,48 @@ class InschrijfForm(FlaskForm):
     voornaam = StringField("Voornaam", validators=[DataRequired()])
     submit = SubmitField('Versturen')
 
-class klantlogin(FlaskForm):
-    email = StringField('Voer hier uw emailadres in',validators=[DataRequired(),email()])
-    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[DataRequired()])
+
+class Klantlogin(FlaskForm):
+    email = StringField('Voer hier uw emailadres in',validators=[data_required(),email()])
+    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
     submit = SubmitField('Inloggen')
 
-class docentlogin(FlaskForm):
-    email = StringField('Voer hier uw emailadres in',validators=[DataRequired(),email()])
-    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[DataRequired()])
+class Docentlogin(FlaskForm):
+    email = StringField('Voer hier uw emailadres in',validators=[data_required(),email()])
+    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
     submit = SubmitField('Inloggen')
+
+
+class Registreren(FlaskForm):
+    email = StringField('Voer hier uw emailadres in',validators=[data_required(),email()])
+    gebruikersnaam = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
+    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required(),EqualTo('herhaalwachtwoord', message='Wachtwoorden moeten overeenkomen')])
+    herhaalwachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
+    submit = SubmitField('Registreren')
+
+    def validate_email(self, field):
+        if Gebruikers.query.filter_by(emailadres=field.data).first():
+            flash('Deze email bestaat al!')
+            return False
+    
+    def validate_username(self, field):
+        if Gebruikers.query.filter_by(gebruikersnaam=field.data).first():
+            flash('Deze gebruikersnaam bestaat al!')
+            return False
+
+class RegistrerenDocent(FlaskForm):
+    email = StringField('Voer hier uw emailadres in',validators=[data_required(),email()])
+    gebruikersnaam = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
+    wachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required(),EqualTo('herhaalwachtwoord', message='Wachtwoorden moeten overeenkomen')])
+    herhaalwachtwoord = StringField('Voer hier uw wachtwoord in', validators=[data_required()])
+    submit = SubmitField('Registreren')
+
+    def validate_email(self, field):
+        if Gebruikers.query.filter_by(emailadres=field.data,is_docent=1).first():
+            flash('Deze email bestaat al!')
+            return False
+    
+    def validate_username(self, field):
+        if Gebruikers.query.filter_by(gebruikersnaam=field.data,is_docent=1).first():
+            flash('Deze gebruikersnaam bestaat al!')
+            return False
